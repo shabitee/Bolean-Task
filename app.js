5e99fb72213ba2569401636157826912b102e49d
shabitee <94993051+shabitee@users.noreply.github.com>
2022-10-16 20:03:04 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+//Exercise 1
+let password = "box";
+if (password > 7);
+{
+    console.log();
+}
+
+//Exercise2
+let height = 160;
+if (typeof height !== "number") {
+    console.log("Please enter a number");
+}    
+
+else if(height >=150){
+console.log("Welcome");
+}
+else{
+    console.log("Sorry");
+}
+
+//Exercise3
+let dice= 6 && 6;
+answer=dice;
+if(answer){
+console.log("You win a bear");
+}
+
+let number=doubleNumber;
+if(doubleNumber){
+    console.log("You win three free throws.");
+
+}
+
+if(dice=evenNumber){
+    console.log("You win one free throw");
+}
+else if(dice=anyOtherCombination){
+    console.log("You loose");
+
+}
